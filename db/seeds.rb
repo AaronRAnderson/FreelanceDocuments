12a9070d392a18533d9ac91aaa49415e618bcc8c
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-06-09 08:21:38 -0600

    generated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create_table "freelance_documents", force: :cascade do |t|
+#   t.string "title"
+#   t.string "description"
+#   t.text "file_url"
+#   t.text "image_url"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+
+10.times do |n|
+  FreelanceDocument.create!(
+    title: "Document #{n+=1}",
+    description: "Praesent commodo cursus magna, vel scelerisque nisl consectetur et.Praesent commodo cursus magna, vel scelerisque nisl consectetur et.",
+    file_url: "https://drive.google.com/file/d/0Bwtm4SZW9BpfMGN4U2NUN1o5RVE/view?usp=sharing",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Document-open.svg/1024px-Document-open.svg.png"
+    )
+end
